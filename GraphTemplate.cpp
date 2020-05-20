6619b615b52b29adcf41df4e0423bf6e4961e42e
Samad Khan <37273365+khansamad99@users.noreply.github.com>
2020-05-20 19:17:59 +0530

    Create GraphTemplate.cpp

---
+++ GraphTemplate.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define ll          long long
+#define pb          push_back
+#define po          pop_back
+#define pii         pair<ll,ll>
+#define vi          vector<ll>
+#define vii         vector<pii>
+#define mi          map<int,int>
+#define ms          map<string,int>
+#define mii         map<pii,int>
+#define all(a)      (a).begin(),(a).end()
+#define x           first
+#define y           second
+#define sz(x)       (int)x.size()
+#define hell        1000000007
+#define pqb         priority_queue<int>
+#define pqs         priority_queue<int,vi,greater<int> >
+#define rep(i,a,b)  for(int i=a;i<b;i++)
+#define repb(i,a,b) for(ll i=a;i>=b;i--)
+#define debug(x,y)  cout<<x<<" "<<y<<"\n";
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+const int N=1e3+5;
+
+vector <int> gr[N];
+int vis[N],ans[N];
+
+void solve()
+{
+    
+}
+int main()
+{
+    IOS
+    int t=1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+}
